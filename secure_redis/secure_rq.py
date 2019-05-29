03150fa4cef07b938b5c3a2f807d8aa9a0144ec3
waseem-omar <waseem.omar@gmail.com>
2019-05-29 16:03:40 +0300

    Remove unneeded python2 code

--- secure_redis/secure_rq.py
+++ secure_redis/secure_rq.py
@@ -1,4 +1,3 @@
-from __future__ import (division, unicode_literals)
 from functools import wraps
 from collections import defaultdict
 import logging
--- secure_redis/tests/test_secure_redis.py
+++ secure_redis/tests/test_secure_redis.py
@@ -1,5 +1,3 @@
-from __future__ import unicode_literals
-
 import django.test
 from django.core import cache
 
